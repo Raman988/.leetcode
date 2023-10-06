cdafb5d7bc798fe7d8d2481f0fb24f643f2d4e9a
Raman988 <75009734+Raman988@users.noreply.github.com>
2023-10-06 04:55:39 +0000

    commit

---
+++ beautifultower.cpp
@@ -0,0 +1,37 @@
+/*
+ * @lc app=leetcode id=2865 lang=cpp
+ *
+ * [2865] Beautiful Towers I
+ */
+
+// @lc code=start
+class Solution {
+public:
+    
+    #define ll long long
+    
+    long long maximumSumOfHeights(vector<int>& a) {
+        int n=a.size();
+        
+       ll int ans=0;
+        for(int i=0;i<n;i++){
+            ll int sum = a[i];
+            int prev=a[i];
+            for(int j=i-1;j>=0;j--){
+                prev = min(a[j],prev);
+                sum += prev;
+            }
+            prev=a[i];
+            for(int j=i+1;j<n;j++){
+                prev = min(a[j],prev);
+                sum += prev;
+            }
+            
+            if(sum > ans) ans=sum;
+        }
+        
+        return ans;
+    }
+};
+// @lc code=end
+
---
+++ beautifultower2.cpp
@@ -0,0 +1,42 @@
+/*
+ * @lc app=leetcode id=2866 lang=cpp
+ *
+ * [2866] Beautiful Towers II
+ */
+
+// @lc code=start
+class Solution {
+public:
+       long long maximumSumOfHeights(vector<int>& A) {
+        int n = A.size();
+        vector<long long> left(n), stack = {-1};
+        long long res = 0, cur = 0;
+        for (int i = 0; i < n; i++) {
+            while (stack.size() > 1 && A[stack.back()] > A[i]) {
+                int j = stack.back();
+                stack.pop_back();
+                cur -= 1L * (j - stack.back()) * A[j];
+            }
+            cur += 1L * (i - stack.back()) * A[i];
+            stack.push_back(i);
+            left[i] = cur;
+        }
+
+        stack = {n};
+        cur = 0;
+        for (int i = n - 1; i >= 0; i--) {
+            while (stack.size() > 1 && A[stack.back()] > A[i]) {
+                int j = stack.back();
+                stack.pop_back();
+                cur -= 1L * -(j - stack.back()) * A[j];
+            }
+            cur += 1L * -(i - stack.back()) * A[i];
+            stack.push_back(i);
+            res = max(res, left[i] + cur - A[i]);
+        }
+
+        return res;
+    }
+};
+// @lc code=end
+
