1ae59cfbd131a9a369e9183dc3a850200c97dc27
Raman988 <75009734+Raman988@users.noreply.github.com>
2023-10-09 14:24:08 +0000

    my commit

---
+++ Day 4/subswithminmumscore.cpp
@@ -0,0 +1,26 @@
+/*
+ * @lc app=leetcode id=2565 lang=cpp
+ *
+ * [2565] Subsequence With the Minimum Score
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int minimumScore(string s, string t) {
+    int ss = s.size(), st = t.size(), k = st - 1;
+    vector<int> dp(st, -1);
+    for (int i = ss - 1; i >= 0 && k >= 0; --i)
+        if (s[i] == t[k])
+            dp[k--] = i;
+    int res = k + 1;
+    for (int i = 0, j = 0; i < ss && j < st && res > 0; ++i)
+        if (s[i] == t[j]) {
+            for (; k < t.size() && dp[k] <= i; ++k);
+            res = min(res, k - (++j));
+        }
+    return res;
+}
+};
+// @lc code=end
+
